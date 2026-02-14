4cb12320dccee817036667e32e66e2bf5b11f4ae
Vansh Bordia <vanshbordia@gmail.com>
2026-02-14 20:08:49 +0530

    updated web

--- betterdocs/app/layout.tsx
+++ betterdocs/app/layout.tsx
@@ -2,7 +2,6 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import SearchDialog from '@/components/search';
-import Script from "next/script";
 import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Layout({ children }: LayoutProps<'/'>) {
@@ -14,7 +13,6 @@ export default function Layout({ children }: LayoutProps<'/'>) {
           body { font-family: 'Google Sans Flex', sans-serif; }
         `}</style>
         <meta name="google-site-verification" content="yVaBi-Tr3-OH7NnWQ8GPL1vbkqC_FOXAEQPlbSLKdsE" />
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-1JK0R2BGKW"></script>
       </head>
       <body className="flex flex-col min-h-screen">
          <GoogleAnalytics gaId="G-1JK0R2BGKW"/>
